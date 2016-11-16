563c293df235620a57495a3520e91ec5f49ec3cd
Nandita Vijaykumar <nandita@cmu.edu>
2016-11-16 18:12:59 -0500

    More fixes

--- launch_tensorflow.py
+++ launch_tensorflow.py
@@ -5,7 +5,7 @@ import sys
 import re
 import subprocess
 import argparse
-
+import time
 
 
 def command(cmd):
@@ -27,8 +27,9 @@ args = parser.parse_args()
 
 #Create VMs
 command("tashi createMany --basename " + args.vm_name + " --cores 8 --memory 8192 --disks 15712-ubuntu-15.10-server-amd64.qcow2,ext3-900GB.qcow2 --count " +  str(args.num_workers + args.num_ps) )
+print("Created VMs.... let's wait for a bit....")
 
-
+time.sleep(30)
 #Create list of ps hosts
 ps_hosts = ""
 for ps in range(args.num_ps):
@@ -39,33 +40,38 @@ for ps in range(args.num_ps):
 
 #Create list of worker hosts
 worker_hosts = ""
-for worker in range(args.num_workers()):
+for worker in range(args.num_workers):
     worker_hosts += args.vm_name + "-" + str(worker) + ":2500"
     if not worker + 1 == args.num_workers:
         worker_hosts += ","
 
 
-for worker in range(args.num_workers()):
-    work_to_do = "git pull base; cd tensorflow/tensorflow/models/image/cifar10; python cifar10_replica.py --ps_hosts=" + ps_hosts + " --worker_hosts=" + worker_hosts + " --job_name=\"worker\" --task_index=" + str(worker) + " --num_gpus=0 --train_steps=10000 --sync_replicas=True &> /mnt/output.log &"
-    os.system("sshpass -p \"tensor\" ssh -o StrictHostKeyChecking=no root@" + args.vm_name + "-" + str(worker) + " '" + str(work_to_do) +"'")
+for worker in range(args.num_workers):
+    work_to_do = "cd tensorflow; git pull base; cd tensorflow/models/image/cifar10; python cifar10_replica.py --ps_hosts=" + ps_hosts + " --worker_hosts=" + worker_hosts + " --job_name=\"worker\" --task_index=" + str(worker) + " --num_gpus=0 --train_steps=10000 --sync_replicas=True &> /mnt/output.log &"
+    print("Worker: " + str(worker))
+    print("ssh -o StrictHostKeyChecking=no root@" + args.vm_name + "-" + str(worker) + " '" + str(work_to_do) +"'")
+    os.system("ssh -o StrictHostKeyChecking=no root@" + args.vm_name + "-" + str(worker) + " '" + str(work_to_do) +"'")
 
-for ps in range(args.num_ps()):
-    work_to_do = "git pull base; cd tensorflow/tensorflow/models/image/cifar10; python cifar10_replica.py --ps_hosts=" + ps_hosts + " --worker_hosts=" + worker_hosts + " --job_name=\"worker\" --task_index=" + str(ps) + " --num_gpus=0 --train_steps=10000 --sync_replicas=True &> /mnt/error.log &"
-    os.system("sshpass -p \"tensor\" ssh -o StrictHostKeyChecking=no root@" + args.vm_name + "-" + str(ps + args.num_workers) + " '" + str(work_to_do) +"'")
+for ps in range(args.num_ps):
+    work_to_do = "cd tensorflow; git pull base; cd tensorflow/models/image/cifar10; python cifar10_replica.py --ps_hosts=" + ps_hosts + " --worker_hosts=" + worker_hosts + " --job_name=\"worker\" --task_index=" + str(ps) + " --num_gpus=0 --train_steps=10000 --sync_replicas=True &> /mnt/error.log &"
+    print("PS: " + str(ps))
+    print("ssh -o StrictHostKeyChecking=no root@" + args.vm_name + "-" + str(ps + args.num_workers) + " '" + str(work_to_do) +"'")
+    os.system("ssh -o StrictHostKeyChecking=no root@" + args.vm_name + "-" + str(ps + args.num_workers) + " '" + str(work_to_do) +"'")
 
 #Monitor
 while True:
-    for node in (args.num_workers + args.num_ps):
+    time.sleep(10) 
+    for node in range(args.num_workers + args.num_ps):
         print("Testing node: " + str(node))
         print("Top says: ")
-        command("sshpass -p \"tensor\" ssh -o StrictHostKeyChecking=no root@" + args.vm_name + "-" +  str(node) + " 'top -b -n2 | grep \"Cpu(s)\"|tail -n 1' > temp.txt")
+        command("ssh -o StrictHostKeyChecking=no root@" + args.vm_name + "-" +  str(node) + " 'top -b -n2 | grep \"Cpu(s)\"|tail -n 1' > temp.txt")
         f = open("temp.txt");
         usage = f.read()
         print(usage)
         f.close()
         print "\n"
         print("Log says: ")
-        command("sshpass -p \"tensor\" ssh -o StrictHostKeyChecking=no root@" + args.vm_name + "-" +  str(node) + " 'cat /mnt/output.log | tail -n 5' > temp.txt")
+        command("ssh -o StrictHostKeyChecking=no root@" + args.vm_name + "-" +  str(node) + " 'cat /mnt/output.log | tail -n 5' > temp.txt")
         f = open("temp.txt")
         log = f.read()
         print(log)
